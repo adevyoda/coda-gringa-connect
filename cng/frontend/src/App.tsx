f2a3c130efd50eca0e93bfc8de137474eb936016
fernandoleopoldino <fernando@expecial.com.br>
2025-05-18 17:23:57 -0300

    test

--- cng/frontend/src/App.tsx
+++ cng/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // Adicione Navigate aqui
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,9 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.MODE === 'development' ? '/cng/' : '/coda-gringa-connect/cng/'}>
+      <BrowserRouter basename={import.meta.env.MODE === 'development' ? '' : '/coda-gringa-connect/cng/'}>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Navigate to="/cng" replace />} /> {/* Adicione este redirecionamento */}
+          <Route path="/cng" element={<Index />} /> {/* Certifique-se de que esta rota esteja correta */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
