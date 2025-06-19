8765234fc42df2d89ac4e5554852ce787a9846d5
fernandoleopoldino <fernando@expecial.com.br>
2025-06-19 17:14:33 -0300

    Alteracao HashRouter dos routers e deploy da esteira

--- cng/frontend/src/App.tsx
+++ cng/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // Adicione Navigate aqui
+import { HashRouter , Routes, Route, Navigate } from "react-router-dom"; // Adicione Navigate aqui
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,13 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.MODE === 'development' ? '' : '/coda-gringa-connect/cng/'}>
+      <HashRouter basename={import.meta.env.MODE === 'development' ? '' : '/coda-gringa-connect/cng/'}>
         <Routes>
           <Route path="/" element={<Navigate to="/cng" replace />} /> {/* Adicione este redirecionamento */}
           <Route path="/cng" element={<Index />} /> {/* Certifique-se de que esta rota esteja correta */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
